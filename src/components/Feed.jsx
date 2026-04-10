4eb5b38da5185504670bd3b23d63f193e973bdd9
varad s kakatkar <varadskakatkar@gmail.com>
2026-04-10 16:44:18 +0530

    intermediate commit,feedds implementation in progress

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { API_URL } from "../constants";
+import { addFeed } from "../utils/feedSlice";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+  const getFeed = async () => {
+    try {
+      if (feed) return;
+      const res = axios.get(`${API_URL}/feed`, { withCredentials: true });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
 
-export default Feed
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return <div>Feed</div>;
+};
+
+export default Feed;
