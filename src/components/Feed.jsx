748c64f6b8afca50b81fd0add84f91dfa3d613e8
varad s kakatkar <varadskakatkar@gmail.com>
2026-04-10 19:35:02 +0530

    User Card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,26 +2,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { API_URL } from "../constants";
 import { addFeed } from "../utils/feedSlice";
 import axios from "axios";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
+import UserCard from "./UserCard";
 
 const Feed = () => {
   const dispatch = useDispatch();
   const feed = useSelector((store) => store.feed);
-  const getFeed = async () => {
+
+  console.log("feed ", feed);
+  const getFeed = useCallback(async () => {
     try {
       if (feed) return;
-      const res = axios.get(`${API_URL}/feed`, { withCredentials: true });
-      dispatch(addFeed(res.data));
+      const res = await axios.get(`${API_URL}/feed`, { withCredentials: true });
+      dispatch(addFeed(res.data?.data || []));
     } catch (error) {
       console.log(error);
       return error;
     }
-  };
+  }, [dispatch, feed]);
 
   useEffect(() => {
     getFeed();
-  }, []);
-  return <div>Feed</div>;
+  }, [getFeed]);
+  return (
+    feed && (
+      <div className="flex justify-center my-10 flex-wrap gap-10 justify-center items-center">
+        {feed.map((user) => (
+          <UserCard key={user._id} user={user} />
+        ))}
+      </div>
+    )
+  );
 };
 
 export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,33 @@
+const UserCard = ({ user }) => {
+  console.log(user);
+  const { firstName, lastName, age, gender, about, photoUrl } = user;
+
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm hover:shadow-lg transition-all duration-300">
+      <figure>
+        <img
+          src={photoUrl}
+          alt={firstName + " " + lastName}
+          className="w-full h-full object-cover rounded-t-lg"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-lg font-bold">
+          {firstName + " " + lastName}
+        </h2>
+        <p>{age + "," + gender}</p>
+        <p className="text-sm text-base-content/70">{about}</p>
+        <div className="card-actions justify-end mt-3 gap-2">
+          <button className="btn btn-secondary btn-outline btn-md">
+            Ignore
+          </button>
+          <button className="btn btn-primary btn-soft btn-md">
+            Interested
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
